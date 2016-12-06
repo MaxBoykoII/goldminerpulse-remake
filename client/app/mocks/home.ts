de5226649c0a001c0384e987f682477f9fc07ac7
maxboyko <maxboykomax@gmail.com>
2016-12-06 20:13:27 +0000

    Center descriptors.

--- client/app/mocks/home.ts
+++ client/app/mocks/home.ts
@@ -4,17 +4,20 @@ export const homeData: Home = {
     tagline: 'Premier Charts and Trend Analysis',
     welcome_statements: ['For gold as well as gold & silver miners on the TSX/TSXV', 'Last Updated With Closing Market Prices and Document Filings from: 26 August 2016 (as available at TSX closing time).'],
     descriptors: [{
-        hook: 'Find clues about the trend',
+        hook: 'Follow trends',
         details: 'Please visit our chart section.'
     }, {
-        hook: 'Learn about the industry',
+        hook: 'Learn the industry',
         details: 'Learn more about important stats by visting our stats page.'
     }, {
-        hook: 'Get updates on developing trends',
+        hook: 'Stay informed',
         details: 'Please visit our featured page.'
     },
     {
-        hook: 'Uncover hidden gems',
+        hook: 'Try our tools',
         details: 'Please visit our tools page'
+    }, {
+        hook: 'Exciting tips',
+        details: 'Frank links'
     }]
 };
